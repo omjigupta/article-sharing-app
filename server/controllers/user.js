81c39880d14501a343b3572cfbc3565b5e99f6f8
omjigupta <omji.mcs.du.2014@gmail.com>
2018-04-15 03:23:37 +0530

    controllers added

---
+++ server/controllers/user.js
@@ -0,0 +1,33 @@
+const User = require('./../models/User')
+
+module.exports = {
+    addUser: (req, res, next) => {
+        new User(req.body).save((err, newUser) => {
+            if (err)
+                res.send(err)
+            else if (!newUser)
+                res.send(400)
+            else
+                res.send(newUser)
+            next()
+        });
+    },
+    getUser: (req, res, next) => {
+        User.findById(req.params.id).then((err, user)=> {
+            if (err)
+                res.send(err)
+            else if (!user)
+                res.send(404)
+            else
+                res.send(user)
+            next()
+        })
+    },
+    followUser: (req, res, next) => {
+        User.findById(req.body.id).then((user) => {
+            return user.follow(req.body.user_id).then(() => {
+                return res.json({msg: "followed"})
+            })
+        }).catch(next)
+    }
+}
--- server/models/User.js
+++ server/models/User.js
@@ -22,5 +22,4 @@ let UserSchema = new mongoose.Schema(
     }
 )
 
-
 module.exports = mongoose.model('User', UserSchema)
